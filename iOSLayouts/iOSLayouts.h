0a4489c748d27fc1ebad0d8be3f7049e24e09b17
Ashish Awasthi <ashish.awasthi2@harman.com>
2020-02-16 20:43:56 +0530

    #01 Added basic sample code

---
+++ iOSLayouts/iOSLayouts.h
@@ -0,0 +1,19 @@
+//
+//  iOSLayouts.h
+//  iOSLayouts
+//
+//  Created by Ashish Awasthi on 16/02/20.
+//  Copyright © 2020 Ashish Awasthi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSLayouts.
+FOUNDATION_EXPORT double iOSLayoutsVersionNumber;
+
+//! Project version string for iOSLayouts.
+FOUNDATION_EXPORT const unsigned char iOSLayoutsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSLayouts/PublicHeader.h>
+
+
